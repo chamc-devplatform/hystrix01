f80922449bedaa3eb1d0aa6abaccaa0682ca787d
zongyingfeng <42405435+zongyingfeng@users.noreply.github.com>
2018-11-06 17:43:52 +0800

    init

---
+++ src/main/java/com/chamc/zong/Hystrix01Application.java
@@ -0,0 +1,12 @@
+package com.chamc.zong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hystrix01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hystrix01Application.class, args);
+	}
+}
---
+++ src/main/java/com/chamc/zong/command/Command.java
@@ -0,0 +1,13 @@
+package com.chamc.zong.command;
+
+/**
+ * 抽象命令角色
+ * @author zongyingfeng
+ *
+ */
+public interface Command {
+    /**
+     * 执行方法
+     */
+    void execute();
+}
